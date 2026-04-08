f8cf7f0d7c858a3daadd01cb6f1579e1edf2784c
Anibal Alvarez <aalvarez@chmw.co>
2026-04-08 16:42:44 -0400

    Initial commit: Modular driver structure

---
+++ src/Auth/TripleWhaleAuthProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Anibalealvarezs\TripleWhaleHubDriver\Auth;
+
+use Anibalealvarezs\ApiSkeleton\Interfaces\AuthProviderInterface;
+
+class TripleWhaleAuthProvider implements AuthProviderInterface
+{
+    private array $credentials = [];
+
+    public function __construct(?array $config = [])
+    {
+        $this->credentials = [
+            'access_token' => $config['triple_whale_api_key'] ?? $_ENV['TRIPLE_WHALE_API_KEY'] ?? '',
+        ];
+    }
+
+    public function getAccessToken(): string
+    {
+        return $this->credentials['access_token'] ?? '';
+    }
+
+    public function isValid(): bool
+    {
+        return !empty($this->getAccessToken());
+    }
+
+    public function isExpired(): bool
+    {
+        return false;
+    }
+
+    public function refresh(): bool
+    {
+        return false;
+    }
+
+    public function getScopes(): array
+    {
+        return [];
+    }
+
+    public function setAuthProvider(AuthProviderInterface $provider): void {}
+}
