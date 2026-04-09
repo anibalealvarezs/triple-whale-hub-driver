70d692b06ee8b824f7c1b71516f36c76d3cfd182
Anibal Alvarez <aalvarez@chmw.co>
2026-04-09 18:45:41 -0400

    Satisfy AuthProviderInterface with updateCredentials implementation

--- src/Auth/TripleWhaleAuthProvider.php
+++ src/Auth/TripleWhaleAuthProvider.php
@@ -41,4 +41,9 @@ class TripleWhaleAuthProvider implements AuthProviderInterface
     }
 
     public function setAuthProvider(AuthProviderInterface $provider): void {}
+
+    public function updateCredentials(array $credentials): void
+    {
+        $this->credentials = array_merge($this->credentials, $credentials);
+    }
 }
