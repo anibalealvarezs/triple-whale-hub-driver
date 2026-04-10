d57b91fd9c13f49bd7798bde301569cbda55b20a
Anibal Alvarez <aalvarez@chmw.co>
2026-04-09 21:40:02 -0400

    Modularize drivers and update dependencies

--- src/Drivers/TripleWhaleDriver.php
+++ src/Drivers/TripleWhaleDriver.php
@@ -8,6 +8,7 @@ use Anibalealvarezs\ApiSkeleton\Traits\HasUpdatableCredentials;
 use Symfony\Component\HttpFoundation\Response;
 use Psr\Log\LoggerInterface;
 use DateTime;
+use Anibalealvarezs\ApiSkeleton\Interfaces\SeederInterface;
 
 class TripleWhaleDriver implements SyncDriverInterface
 {
@@ -79,5 +80,13 @@ class TripleWhaleDriver implements SyncDriverInterface
     public function validateConfig(array $config): array
     {
         return $config;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function seedDemoData(SeederInterface $seeder, array $config = []): void
+    {
+        // Placeholder for future implementation
     }
 }
